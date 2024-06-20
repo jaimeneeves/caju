51a6c3e720fe758fcf02035e7f6c2d7cfd67f8d7
Jaime Neves <jaimebarrosoneves@gmail.com>
2024-06-20 18:17:36 -0300

    implementado o get da página inicial [dashboard]

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from '~/api/axios-config';
 import Collumns from "./components/Columns";
 import * as S from "./styles";
 import { SearchBar } from "./components/Searchbar";
 
 const DashboardPage = () => {
+  const [registrations, setRegistrations] = useState([]);
+
+  useEffect(() => {
+    const fetchRegistrations = async () => {
+      try {
+        const response = await axios.get('/registrations');
+        setRegistrations(response.data);
+      } catch (error) {
+        console.error("Error fetching registrations:", error);
+      }
+    };
+
+    fetchRegistrations();
+  }, []);
+
   return (
     <S.Container>
       <SearchBar />
-      <Collumns registrations={[]} />
+      <Collumns registrations={registrations} />
     </S.Container>
   );
 };
