bbec249787e45d78241eea8afa24c98137190fbf
Jaime Neves <jaimebarrosoneves@gmail.com>
2024-06-21 16:08:59 -0300

    implementando o GET ao fazer pesquisa por CPF

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -1,20 +1,31 @@
 import { HiRefresh } from "react-icons/hi";
 import { useHistory } from "react-router-dom";
+import { ChangeEvent } from 'react';
+
 import Button from "~/components/Buttons";
 import { IconButton } from "~/components/Buttons/IconButton";
 import TextField from "~/components/TextField";
 import routes from "~/router/routes";
 import * as S from "./styles";
-export const SearchBar = () => {
+
+type SearchBarProps = {
+  handleSearch: (term: string) => void;
+}
+
+export const SearchBar = ({ handleSearch }: SearchBarProps) => {
   const history = useHistory();
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    handleSearch(event.target.value);
+  };
   
   return (
     <S.Container>
-      <TextField  placeholder="Digite um CPF válido" />
+      <TextField  placeholder="Digite um CPF válido" onChange={handleInputChange} />
       <S.Actions>
         <IconButton aria-label="refetch">
           <HiRefresh />
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,27 +3,56 @@ import axios from '~/api/axios-config';
 import Collumns from "./components/Columns";
 import * as S from "./styles";
 import { SearchBar } from "./components/Searchbar";
+import { useDebounce } from '~/hooks/useDebounce';
+
+type Registration = {
+  id: string;
+  admissionDate: string;
+  email: string;
+  employeeName: string;
+  status: string;
+  cpf: string;
+}
 
 const DashboardPage = () => {
-  const [registrations, setRegistrations] = useState([]);
+  const [registrations, setRegistrations] = useState<Registration[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const fetchRegistrations = async (cpf?: string) => {
+    try {
+      const response = await axios.get('/registrations', {
+        params: cpf ? { cpf } : {} });
+      setRegistrations(response.data);
+    } catch (error) {
+      console.error("Error fetching registrations:", error);
+    }
+  };
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+  };
+
+  const filteredRegistrations = registrations.filter(registration =>
+    registration.cpf.includes(searchTerm)
+  );
 
   useEffect(() => {
-    const fetchRegistrations = async () => {
-      try {
-        const response = await axios.get('/registrations');
-        setRegistrations(response.data);
-      } catch (error) {
-        console.error("Error fetching registrations:", error);
-      }
-    };
-
     fetchRegistrations();
   }, []);
 
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      fetchRegistrations(debouncedSearchTerm);
+    } else {
+      fetchRegistrations();
+    }
+  }, [debouncedSearchTerm]);
+
   return (
     <S.Container>
-      <SearchBar />
-      <Collumns registrations={registrations} />
+      <SearchBar handleSearch={handleSearch} />
+      <Collumns registrations={filteredRegistrations} />
     </S.Container>
   );
 };
